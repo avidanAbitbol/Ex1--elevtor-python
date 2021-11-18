ba776771b6110bdc73b6897a6f53001a1f89182a
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-18 15:36:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from building import *
-from elevators import *
-from calls import *
-from elevators import Elevators
+from Calls import *
+from Elevators import Elevators
+from Building import Building
+from Calls import load_csv_calls
 
 
-def time_for_call(elv=Elevators, call=Callas):
+def playTime(elv=Elevators, call=Calls): #time for each call(acording to elevator param.
     speed = float(elv.speed)
     open = float(elv.open_time)
     close = float(elv.close_time)
@@ -12,68 +12,59 @@ def time_for_call(elv=Elevators, call=Callas):
     stop = float(elv.stop_time)
     src = int(call.src_floor)
     dst = int(call.dst_floor)
-    time_to_complete = (abs(src - dst) / speed) + (2 * (open + close) + (start + stop))
-    return time_to_complete
+    playTime = open+close+start+(abs(src - dst) / speed)+stop+open+close
+    return playTime
 
 
-def unbusy_elv(building=Building, call=Callas):
-    el = building.elvators
-    elv_id = -1
-    min_time = 9223372036854775807
-    for t in range(len(el)):
-        if len(el[t].time_busy) <= 0:
-            temp = time_for_call(el[t], call)
-            if temp < min_time:
-                min_time = temp
-                elv_id = t
-    return elv_id
+def isEmpty(elev=Elevators):#check if the elev. have no calls
+     if len(elev.elevCalls) <= 0:
+            return True
 
-
-def add_time_busy(elv=Elevators, call=Callas):
-    elv.time_busy.append(time_for_call(elv, call) + float(call.time))
-
-
-def delete_busy(bui=Building, call=Callas):
-    for q in bui.elvators:
-        if len(q.time_busy) > 0:
-            for busy in q.time_busy:
+def doneCall(myBuild=Building, call=Calls):
+    for q in myBuild.elvators:
+        if len(q.elevCalls) > 0:
+            for busy in q.elevCalls:
                 if busy < float(call.time):
-                    q.time_busy.pop(q.time_busy.index(busy))
+                    q.elevCalls.pop(q.elevCalls.index(busy))
 
 
-def allocate(build=Building, call=Callas):
-    chosen_elv = -1
-    min_time = 9223372036854775807
-    delete_busy(build, call)
-    free_elev = unbusy_elv(build, call)
-    if free_elev != -1:
-        call.elv_id = free_elev
-        build.elvators[free_elev].calls_for_elv.append(call)
-        add_time_busy(build.elvators[free_elev], call)
-        return free_elev
+def allocate(myBuild=Building, call=Calls):
+    ElevId = -1
+    min = 11111111111111
+    doneCall(myBuild, call)#erase calls that done until this new one appeared
+
+    for i in range(len(myBuild.elvators)):
+        if isEmpty(myBuild.elvators[i]):#if there's no calls, sort and return the best enpty elev. for this call
+            temp = playTime(myBuild.elvators[i],call)
+            if temp<min:
+                min=temp
+                ElevId=i
+            myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))
+            call.elev_id = ElevId
+            return ElevId
+
     else:
-        for r in range(len(build.elvators)):
-            temp = abs(build.elvators[r].time_busy[-1] - float(call.time))
-            if temp < min_time:
-                min_time = temp
-                chosen_elv = r
-        call.elv_id = chosen_elv
-        build.elvators[chosen_elv].calls_for_elv.append(call)
-        add_time_busy(build.elvators[chosen_elv], call)
-    return chosen_elv
+      for i in range(len(myBuild.elvators)):
+            temp = abs(myBuild.elvators[i].elevCalls[-1] - float(call.time))
+            if temp < min:
+                min = temp
+                ElevId = i
+            call.elv_id = ElevId
+            myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))#push new call to choosen elev. list
+            return ElevId
 
 
 def output(file_build, file_calls, file_update_calls):
-    b = Building(0, 0)
-    b.load_json_build(file_build)
+    myBuild = Building(0, 0)
+    myBuild.load_json_build(file_build)
     list_calls = load_csv_calls(file_calls)
     update_calls = []
     for i in list_calls:
-        allocate(b, i)
+        allocate(myBuild, i)
         update_calls.append(i.__dict__.values())
     with open(file_update_calls, 'w', newline="") as f:
         csw = csv.writer(f)
         csw.writerows(update_calls)
     return file_update_calls
 
-output("B4.json", "Calls_c.csv", "output.csv")
+output("B1.json", "Calls_a.csv", "output.csv")
