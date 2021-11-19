848d6a120d8ebb94dfc10269fc0119c9a4400425
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-19 23:42:31 +0200

    Update Elevators.py

--- Elevators.py
+++ Elevators.py
@@ -1,9 +1,6 @@
-from Calls import Calls
 
-
-class Elevators:
-    def __init__(self, id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time,
-                 calls_for_elv, elevCalls):
+class Elevator:
+    def __init__(self, id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time, elevCalls):
         self.id = id
         self.speed = speed
         self.min_floor = min_floor
@@ -12,11 +9,9 @@ class Elevators:
         self.open_time = open_time
         self.start_time = start_time
         self.stop_time = stop_time
-        self.calls_for_elv = calls_for_elv
         self.elevCalls = elevCalls
 
     def __repr__(self) -> str:
         return f"repr _id:{self.id} speed:{self.speed} min_floor:{self.min_floor} " \
                f"max_floor:{self.max_floor} close_time:{self.close_time} " \
-               f"open_time:{self.open_time} start_time:{self.stop_time} stop_time:{self.stop_time} calls_for_elv:{self.calls_for_elv}" \
-               f"elevCalls:{self.elevCalls} "
+               f"open_time:{self.open_time} start_time:{self.stop_time} stop_time:{self.stop_time} time_busy:{self.elevCalls}" \
