89f16afab1eeda7dbda6b218bca58d89d8eeeab7
avidan.abitbol <avidan888@gmail.com>
2021-11-19 09:21:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 from Calls import *
 from Elevators import Elevators
-from Building import Building
-from Calls import load_csv_calls
+from building import Building
+from Calls import load_csv_calls, Calls
 
 
-def playTime(elv=Elevators, call=Calls): #time for each call(acording to elevator param.
+def playTime(elv=Elevators, call=Calls):  # time for each call(according to elevator param)
     speed = float(elv.speed)
     open = float(elv.open_time)
     close = float(elv.close_time)
@@ -13,13 +12,15 @@ def playTime(elv=Elevators, call=Calls): #time for each call(acording to elevato
     stop = float(elv.stop_time)
     src = int(call.src_floor)
     dst = int(call.dst_floor)
-    playTime = open+close+start+(abs(src - dst) / speed)+stop+open+close
+    playTime = open + close + start + (abs(src - dst) / speed) + stop + open + close
     return playTime
 
 
-def isEmpty(elev=Elevators):#check if the elev. have no calls
-     if len(elev.elevCalls) <= 0:
-            return True
+# check if the elev. have no calls
+def isEmpty(elev=Elevators):
+    if len(elev.elevCalls) <= 0:
+        return True
+
 
 def doneCall(myBuild=Building, call=Calls):
     for i in myBuild.elvators:
@@ -28,45 +29,41 @@ def doneCall(myBuild=Building, call=Calls):
                 if busy < float(call.time):
                     i.elevCalls.pop(i.elevCalls.index(busy))
 
-def sortEmpty(emptyElevs=Elevators,call=Calls):
-    ElevId=-1
+
+def sortEmpty(emptyElevs=Elevators, call=Calls):
+    ElevId = -1
     min = 11111111111111
-    for i in range (len(emptyElevs)):
-          temp = playTime(emptyElevs[i], call)
-          if temp < min:
+    for i in range(len(emptyElevs)):
+        temp = playTime(emptyElevs[i], call)
+        if temp < min:
             min = temp
             ElevId = i
     return ElevId
 
+
 def allocate(myBuild=Building, call=Calls):
     ElevId = -1
     min = 11111111111111
-    doneCall(myBuild, call)#erase calls that done until this new one appeared
-
+    # erase calls that done until this new one appeared
+    doneCall(myBuild, call)
     for i in range(len(myBuild.elvators)):
-
-        if isEmpty(myBuild.elvators[i]):  #if there's no calls, sort and return the best enpty elev. for this call
-            emptyElevs = []
-            emptyElevs.append(myBuild.elvators[i])
-
-        else:
-            for i in range(len(myBuild.elvators)):
-              temp = abs(myBuild.elvators[i].elevCalls[-1] - float(call.time))
-              if temp < min:
-                min = temp
+        # if there's no calls, sort and return the best empty elev. for this call
+        if isEmpty(myBuild.elvators[i]):
+            time = playTime(myBuild.elvators[i], call)
+            if time < min:
+                min = time
                 ElevId = i
-            call.elv_id = ElevId
-            myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))#push new call to choosen elev. list
-
-
-            if len(emptyElevs)>0:
-                ElevId = sortEmpty()
-                myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))
-                call.elev_id = ElevId
-                return ElevId
-            else:
-                return ElevId
-
+        call.elev_id = ElevId
+        myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))
+    else:
+        temp = abs(myBuild.elvators[i].elevCalls[-1] - float(call.time))
+        if temp < min:
+            min = temp
+            ElevId = i
+        call.elv_id = ElevId
+        # push new call to chosen elev. list
+        myBuild.elvators[ElevId].elevCalls.append(playTime(myBuild.elvators[ElevId], call) + float(call.time))
+        return ElevId
 
 
 def output(file_build, file_calls, file_update_calls):
@@ -82,4 +79,6 @@ def output(file_build, file_calls, file_update_calls):
         csw.writerows(newCalls)
     return file_update_calls
 
-output("B5.json", "Calls_d.csv", "output.csv")
+
+output("B3.json", "Calls_c.csv", "output.csv")
+
