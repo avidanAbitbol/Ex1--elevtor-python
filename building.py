c5fdabed836d33f849432949a548d3e6e73e75c9
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-19 23:41:57 +0200

    Update building.py

--- building.py
+++ building.py
@@ -1,30 +1,24 @@
 import json
-import sys
-from Calls import Calls
-from Elevators import Elevators
+from Elevator import Elevator
 
 
 class Building(object):
-    elvators = [Elevators]
+    elvators = [Elevator]
 
-    def __init__(self, min_floor, max_floor, elvators=[Elevators]):
+    def __init__(self, min_floor, max_floor, elvators=[Elevator]):
         self.min_floor = min_floor
         self.max_floor = max_floor
         self.elvators = elvators
 
-    def load_json_build(self, file):
-        elvator_list = []
+    def BuildFromJason(self, file):
+        elevators = []
         with open(file, "r") as f:
-            d = json.load(fp=f)
-            self.min_floor = d["_minFloor"]
-            self.max_floor = d["_maxFloor"]
-            for elev in d["_elevators"]:
-                el = Elevators(id=elev["_id"], speed=elev["_speed"], min_floor=elev["_minFloor"],
-                               max_floor=elev["_maxFloor"],
-                               close_time=elev["_closeTime"], open_time=elev["_openTime"],
-                               start_time=elev["_startTime"],
-                               stop_time=elev["_stopTime"], calls_for_elv=[], elevCalls=[])
-                elvator_list.append(el)
-            self.elvators = elvator_list
+            B = json.load(fp=f)
+            self.min_floor = B["_minFloor"]
+            self.max_floor = B["_maxFloor"]
+            for elev in B["_elevators"]:
+                el = Elevator(id=elev["_id"], speed=elev["_speed"], min_floor=elev["_minFloor"],max_floor=elev["_maxFloor"], close_time=elev["_closeTime"], open_time=elev["_openTime"],start_time=elev["_startTime"],stop_time=elev["_stopTime"], elevCalls=[])
+                elevators.append(el)
+            self.elvators = elevators
 
         return self
